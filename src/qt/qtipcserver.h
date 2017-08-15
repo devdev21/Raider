4e39ad897f00132f1ddc401d88ce8e515b02df2e
DEVDEV <70098@emailn.de>
2017-08-15 20:42:41 +0200

    RAI
    RAI

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define Raider-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "RaiderURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
